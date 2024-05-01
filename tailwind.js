2af0fa10454769786405f0610b059ff14701d2bb
muhammadega21 <dermawane988@gmail.com>
2024-05-01 15:56:09 +0700

    home

---
+++ tailwind.js
@@ -0,0 +1,18 @@
+tailwind.config = {
+  theme: {
+    screens: {
+      a_sm: "390px",
+      a_md: "490px",
+      a_lg: "590px",
+      sm: "640px",
+
+      md: "966px",
+
+      lg: "1140px",
+
+      xl: "1260px",
+
+      "2xl": "1536px",
+    },
+  },
+};
