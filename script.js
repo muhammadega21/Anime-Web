030d775f1528349a4ca455ef51869d32d66d7faa
muhammadega21 <dermawane988@gmail.com>
2024-05-05 23:53:12 +0700

    test api

---
+++ script.js
@@ -0,0 +1,38 @@
+document.addEventListener("alpine:init", () => {
+  Alpine.store("anime", {
+    ongoingDatas: [],
+    ongoingEpisodesDatas: [],
+
+    ongoingFetch() {
+      fetch("https://otakudesu-unofficial-api.rzkfyn.xyz/v1/home")
+        .then((response) => response.json())
+        .then((ongoingAnimesData) => {
+          this.ongoingDatas = ongoingAnimesData.data.ongoing_anime;
+
+          const episodePromises = ongoingAnimesData.data.ongoing_anime.map(
+            (anime) => {
+              const slug = anime.slug;
+              return fetch(
+                `https://otakudesu-unofficial-api.rzkfyn.xyz/v1/anime/${slug}/episodes`
+              )
+                .then((response) => response.json())
+                .then((episodesData) => ({
+                  anime,
+                  episodes: episodesData.data.reverse().slice(0, 6),
+                }));
+            }
+          );
+
+          Promise.all(episodePromises).then((episodesData) => {
+            this.ongoingEpisodesDatas = episodesData;
+            console.log(episodesData);
+          });
+        });
+    },
+  });
+});
+
+function getEpisodeNumber(urls) {
+  const match = urls.match(/(?:episode|ep)-(\d+)/i);
+  return match ? `Episode ${match[1]}` : null;
+}
